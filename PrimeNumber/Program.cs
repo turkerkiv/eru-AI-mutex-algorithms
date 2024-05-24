a10a1a0e597e00dc4b2b9c641ee8fcb9f7ef17f3
turkerkiv <99351028+turkerkiv@users.noreply.github.com>
2024-05-24 13:27:40 +0300

    made multithreading work faster by threadpooling

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -17,32 +17,41 @@ internal class Program
             return;
         }
 
-        num1 = num1 < 2 ? 2 : num1;
+        num1 = num1 < 3 ? 3 : num1;
 
         List<Thread> threads = new List<Thread>();
         var watch = new System.Diagnostics.Stopwatch();
         watch.Start();
 
-        // for (int i = num1; i <= num2; i++)
-        // {
-        //     //araştırınca thread kullanırken anonim fonksiyon içinde methoda parametre verince valuetype bile olsa reference gönderiyormuş.
-        //     //yani localI tanımlamazsam birçok sorun çıkıyor. Mesela aynı sayının 2 3 defa prime mı diye kontrol edilmesi ve print edilmesi.
-        //     int localI = i;
-        //     Thread checkPrimeThread = new Thread(() => PrintIfPrime(localI));
-        //     checkPrimeThread.Start(); //foreach içinde joinden hemen önce çağırınca performans olarak kötüleşiyor. burda yazınca performans daha iyi.
-        //     threads.Add(checkPrimeThread);
-        // }
+        System.Console.Write("2 ");
+        for (int i = num1; i <= num2; i += 2)
+        {
+            //------------------------------------- 1. YÖNTEM -------------------------------------
+            //araştırınca thread kullanırken anonim fonksiyon içinde methoda parametre verince valuetype bile olsa reference gönderiyormuş.
+            //yani localI tanımlamazsam birçok sorun çıkıyor. Mesela aynı sayının 2 3 defa prime mı diye kontrol edilmesi ve print edilmesi.
+            int localI = i;
+            // Thread checkPrimeThread = new Thread(() => PrintIfPrime(localI));
+            // checkPrimeThread.Start(); //foreach içinde joinden hemen önce çağırınca performans olarak kötüleşiyor. burda yazınca performans daha iyi.
+            // threads.Add(checkPrimeThread);
 
+            //------------------------------------- 2. YÖNTEM -------------------------------------
+            ThreadPool.QueueUserWorkItem((state) => PrintIfPrime(localI));
+        }
+
+        Console.Read(); //bunun yerine bir şey koymak lazım ki program kapanmasın. çünkü threadlerin hepsi çalışmamış olabilir. CountdownEvent.Wait() olabilir belki
+
+        //------------------------------------- 1. YÖNTEM DEVAMI-------------------------------------
         // foreach (var thread in threads)
         // {
-        //     // thread.Start();
-        //     thread.Join();
+        // thread.Start();
+        // thread.Join();
         // }
 
-        Parallel.For(num1, num2 + 1, i =>
-        {
-            PrintIfPrime(i);
-        });
+        //------------------------------------- 3. YÖNTEM -------------------------------------
+        // Parallel.For(num1, num2 + 1, i =>
+        // {
+        //     PrintIfPrime(i);
+        // });
 
         System.Console.WriteLine();
         watch.Stop();
@@ -53,26 +62,13 @@ internal class Program
     {
         bool isPrime = true;
 
-        if (num == 2)
+        int sqrtNum = (int)Math.Sqrt(num);
+        for (int i = 3; i <= sqrtNum; i += 2)
         {
-            isPrime = true;
-            return;
-        }
-
-        if (num % 2 != 0)
-        {
-            int sqrtNum = (int)Math.Sqrt(num);
-            for (int i = 3; i <= sqrtNum; i += 2)
+            if (num % i == 0)
             {
-                if (num % i == 0)
-                {
-                    isPrime = false;
-                }
+                isPrime = false;
             }
-        }
-        else
-        {
-            isPrime = false;
         }
 
         if (isPrime)
