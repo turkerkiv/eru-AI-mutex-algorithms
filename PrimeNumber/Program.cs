feb926fb3395e329133994bf7d7d757e8330f502
turkerkiv <99351028+turkerkiv@users.noreply.github.com>
2024-05-31 16:07:53 +0300

    bug fix
    prints 2 without looking beginning of the range

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -17,13 +17,21 @@ internal class Program
             return;
         }
 
-        num1 = num1 < 3 ? 3 : num1;
+        if (num1 > num2)
+        {
+            System.Console.WriteLine("The beginning of the range must be less than the end of the range.");
+            return;
+        }
+
+        if (num1 < 3)
+        {
+            num1 = 3;
+            System.Console.Write("2 ");
+        }
 
         List<Thread> threads = new List<Thread>();
         var watch = new System.Diagnostics.Stopwatch();
         watch.Start();
-
-        System.Console.Write("2 ");
 
         //------------------------------------- 3. YÖNTEM -------------------------------------
         Parallel.ForEach(Enumerable.Range(num1, num2 + 1).Where(i => i % 2 != 0), i =>
